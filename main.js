cc647c308c77b1344b6f206e6fbf752a0832c5e9
JIAtype <e1285215@u.nus.edu>
2023-10-17 11:44:45 +0800

    add index

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const express = require('express')
 const {engine} = require ('express-handlebars')
 
 //port to listen to
-const POST = parseInt(process.env.PORT) || 3000
+const PORT = parseInt(process.env.PORT) || 3000
 
 //Create an instance of express
 const app = express()
 
+//Serve files in the static directory
+app.use(express.static(__dirname+'/static'))
+
 app.listen(PORT, () => {
-    console.info(`Application strated on port ${PORT} at ${new Date()}`)
+    console.info(`Application strated on port ${PORT} at ${new Date()}`)
 })
